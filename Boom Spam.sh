c6553bbbb89196415425a3267179ecfd874ce949
Fatin <39091723+XxXKanekiXxX@users.noreply.github.com>
2018-10-18 00:58:29 +0700

    Add files via upload

---
+++ Boom Spam.sh
@@ -0,0 +1,109 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "Boom Spam"
+sleep 1
+echo "\033[31;1m Macam-macam tool untuk spam"
+sleep 1
+echo "\033[33;1m Author: XxXKanekiXxX"
+sleep 1
+echo "\033[32;1m contack: 085740462770"
+sleep 1
+echo "\033[34;1m Sharing Gils Blog"
+sleep 1
+echo "\033[36;1m Spam Yang Tersedia :"
+echo "1. Tokopedia"
+echo "2. Telkomsel"
+echo "3. Matahari Mall"
+echo "4. PHD"
+echo "5. Jd.Id"
+echo "6. Email Bomber"
+echo "0. Keluar"
+echo "\033[30;1m81. Informasi Tool Ini"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[34;1m"
+figlet "tokopedia"
+php 1.php
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "T-Sel"
+php 2.php
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+echo "\033[31;1m"
+figlet "Matahari Mall"
+php 3.php
+fi
+
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f mono12 -F gay "PHD"
+echo "\033[30;1m"
+php 4.php
+fi
+
+if
+[ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+toilet -f mono12 -F gay "Jd.Id"
+echo "\033[33;1m"
+php 5.php
+fi
+
+if
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+then
+clear
+toilet -f standard -F gay "Email Bomber"
+echo "\033[36;1m"
+python2 6.py
+fi
+
+if
+[ $mrrm = 81 ] || [ $mrrm = 81 ]
+then
+clear
+toilet -f slant --gay "Boom Spam"
+echo "\033[31;1mNama tools: Boom Spam"
+sleep 1
+echo "\033[33;1mKarya: XxXKanekiXxX (v1 By .....)"
+sleep 1
+echo "\033[32;1mVersi: v2"
+sleep 1
+echo "\033[36;1mTeam: Sharing Gils Blog"
+sleep 1
+echo "\033[34;1mInformasi Lebih Lanjut Kunjungi: https://exploit45.blogspot.com"
+echo "\033[31;1m Spesial Thanks To: "
+echo "\033[36;1m"
+echo "Friends"
+echo "Babby Cyber Team"
+echo "SBG-Team"
+sleep 1
+echo "\033[30;1m tunggu 8 detik"
+sleep 8
+sh LITESPAM.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m See You Next Time"
+sleep 1
+fi
+
